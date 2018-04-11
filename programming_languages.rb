d14a2ca411c45b8dcaa42c64fd252e8a8b86a32e
Michelle Blevins <mjblevins7@gmail.com>
2018-04-11 03:30:47 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -42,7 +42,8 @@ def reformat_languages(languages)
         end
         languages_in_order[language] = {type => type_value}
         if languages_in_order[language][:style] = nil
-          languages_in_order[language][:style] = [] << style
+          languages_in_order[language][:style] = []
+          binding.pry
         end
       end
     end
