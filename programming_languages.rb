65148fbdd9048eefd4fd7837f01215ab7ab0eebf
Michelle Blevins <mjblevins7@gmail.com>
2018-04-11 03:57:23 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -43,7 +43,7 @@ def reformat_languages(languages)
         languages_in_order[language] = {type => type_value}
         languages_in_order[language][:style] = Array.new
         (languages_in_order[language][:style]) << style
-        if !(languages_in_order[language][:style]).include?(style)
+        if (languages_in_order[language][:style]).include?(style) === false
           (languages_in_order[language][:style]).push(style)
         end
       end
