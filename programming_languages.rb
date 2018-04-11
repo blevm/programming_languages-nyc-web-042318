d505cedd5b47b5c5efb208eb6b8c35b19bfdeea3
Michelle Blevins <mjblevins7@gmail.com>
2018-04-11 03:25:25 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,50 @@
+require "pry"
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  languages_in_order = {}
+  languages.each do |style, language|
+    language.each do |language, type|
+      type.each do |type, type_value|
+        if languages_in_order[language] = nil
+          languages_in_order[language] = {}
+        end
+        languages_in_order[language] = {type => type_value}
+      end
+    end
+  end
+  return languages_in_order
 end
+
+binding.pry
